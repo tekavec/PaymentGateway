f8ccacbdb9e2958fe6da92ab56ee14c12cc19c37
tekavec <tomaz.tekavec@gmail.com>
2019-06-02 13:44:33 +0100

    Add test project with one dummy test

---
+++ test/PaymentGateway.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace PaymentGateway.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            1.Should().Be(1);
+        }
+    }
+}
